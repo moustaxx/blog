df10433891aeaf76f069a54a1cc36d91e7265cea
moustaxx <maciasm1303@gmail.com>
2019-08-21 17:58:32 +0200

    Edit TopBar, add nav links

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -5,23 +5,44 @@ import { makeStyles } from '@material-ui/styles';
 const useStyles = makeStyles({
 	root: {
 		width: '100%',
+		position: 'absolute',
 		height: 48,
 		top: 0,
-		backgroundColor: 'white',
+		backgroundColor: '#2b2523',
 	},
 	container: {
 		height: '100%',
-		maxWidth: 1156,
+		maxWidth: 1088,
 		padding: '0 32px',
 		margin: '0 auto',
 		display: 'flex',
+		justifyContent: 'space-between',
 		alignItems: 'center',
 	},
 	title: {
-		fontSize: '1.2rem',
 		fontWeight: 800,
+		color: 'white',
+	},
+	navBar: {
+		height: '100%',
+		display: 'flex',
+	},
+	link: {
 		textDecoration: 'none',
-		color: 'blueviolet',
+		textShadow: 'none',
+		background: 'none',
+	},
+	navElement: {
+		height: '100%',
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+		color: 'white',
+		padding: '0 12px',
+		'&:hover': {
+			color: 'hsl(251, 100%, 76%)',
+			transition: '.2s color',
+		},
 	},
 }, { name: 'TopBar' });
 
@@ -31,7 +52,11 @@ const TopBar = () => {
 	return (
 		<div className={classes.root}>
 			<div className={classes.container}>
-				<Link className={classes.title} to="/">Personal Blog</Link>
+				<Link className={`${classes.link} ${classes.title}`} to="/">Personal Blog</Link>
+				<nav className={classes.navBar}>
+					<Link className={`${classes.link} ${classes.navElement}`} to="/blog">Blog</Link>
+					<Link className={`${classes.link} ${classes.navElement}`} to="/about">About</Link>
+				</nav>
 			</div>
 		</div>
 	);
