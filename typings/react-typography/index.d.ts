acdce1da7881d7156b9975900b131402c2df845c
moustaxx <maciasm1303@gmail.com>
2019-09-26 17:19:55 +0200

    Add react-typography type definitions

---
+++ typings/react-typography/index.d.ts
@@ -0,0 +1,10 @@
+declare module 'react-typography' {
+	import Typography from 'typography';
+
+	interface IProps {
+		typography: Typography;
+	}
+
+	export declare const TypographyStyle = (props: IProps) => any;
+	export declare const GoogleFont = (props: IProps) => any;
+}
